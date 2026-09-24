5e230d43001a0c1bc4ed96b373046bb7b2e608f9
Rameez Baig <mirzarameezahmedbaig@gmail.com>
2026-09-24 13:36:27 -0700

    added warning message for missing restaurants file; added custom json decode error message; added custom pydantic validation error message; now reading restaurants.json with utf-8 encoding to account for special characters

--- backend/app/repositories/restaurant_repository.py
+++ backend/app/repositories/restaurant_repository.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
-from pydantic import TypeAdapter
+from json import JSONDecodeError
+from pydantic import TypeAdapter, ValidationError
 
 from backend.app.schemas.restaurant import Restaurant
 
@@ -12,11 +13,16 @@ class RestaurantRepository:
 
     def __load_restaurants(self) -> list[Restaurant]: 
         if not self.restaurants_file_path.exists():
+            print(f"[WARNING] '${self.restaurants_file_path.absolute()}' does not exist. Defaulting to empty restaurants list.")
             return []
 
-        with open(self.restaurants_file_path, "r") as restaurants_file:
-            restaurants_list = json.load(restaurants_file)
-            return restaurants_list_adapter.validate_python(restaurants_list)
+        with open(self.restaurants_file_path, "r", encoding="utf-8") as restaurants_file:
+            try:
+                return restaurants_list_adapter.validate_python(json.load(restaurants_file))
+            except JSONDecodeError:
+                raise Exception(f"{self.restaurants_file_path.absolute()} is not JSON serializable.")
+            except ValidationError:
+                raise Exception(f"{self.restaurants_file_path.absolute()} does not follow the correct list[Restaurant] schema. See backend/app/schemas/Restaurant.py or visit https://www.schema.org/Restaurant for schema specifications.")
 
     def get_all_restaurants(self) -> list[Restaurant]:
         return self.__load_restaurants()
