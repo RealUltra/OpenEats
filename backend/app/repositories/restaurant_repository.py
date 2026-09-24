55a97f15156b931fd3d86cdae9e62703b656712a
Rameez Baig <mirzarameezahmedbaig@gmail.com>
2026-09-24 16:02:30 -0700

    now raising ValueError on ValidationError since ValidationError cannot be raised with a message

--- backend/app/repositories/restaurant_repository.py
+++ backend/app/repositories/restaurant_repository.py
@@ -22,7 +22,7 @@ class RestaurantRepository:
             except JSONDecodeError as e:
                 raise JSONDecodeError(f"{self.restaurants_file_path.absolute()} is not JSON serializable.", e.doc, e.pos) from e
             except ValidationError as e:
-                raise ValidationError(f"{self.restaurants_file_path.absolute()} does not follow the correct list[Restaurant] schema. See backend/app/schemas/restaurant.py or visit https://www.schema.org/Restaurant for schema specifications.") from e
+                raise ValueError(f"{self.restaurants_file_path.absolute()} does not follow the correct list[Restaurant] schema. See backend/app/schemas/restaurant.py or visit https://www.schema.org/Restaurant for schema specifications.") from e
 
     def get_all_restaurants(self) -> list[Restaurant]:
         return self.__load_restaurants()
